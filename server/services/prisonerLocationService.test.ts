eb0237534d7251ab0f27740c9a8b0967c1802091
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-05-06 10:07:46 +0100

    SDIT-3774: 🚨 Fix compiler warnings for strict tsc mode (#113)

--- server/services/prisonerLocationService.test.ts
+++ server/services/prisonerLocationService.test.ts
@@ -24,7 +24,7 @@ describe('Prisoner Location service', () => {
 
       const result = await prisonerLocationService.todaysFile(token)
 
-      expect(result.name).toEqual('john smith')
+      expect(result?.name).toEqual('john smith')
     })
 
     it('Propagates error', async () => {
